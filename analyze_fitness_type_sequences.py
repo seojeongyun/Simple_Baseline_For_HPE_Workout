0a43cd739ce988919aa8b96ec16da6aa9c835d36
SeoJeongYun <tjwjddbs4018@naver.com>
2025-07-20 17:34:06 +0900

    .

--- analyze_fitness_type_sequences.py
+++ analyze_fitness_type_sequences.py
@@ -7,6 +7,7 @@ import numpy as np
 from glob import glob
 import fnmatch
 from tqdm import tqdm
+from collections import defaultdict
 
 if __name__ == '__main__':
     # *-*-*-*-*-*- Collect json files from validation dataset *-*-*-*-*-*-
@@ -15,7 +16,8 @@ if __name__ == '__main__':
 
     json_list_valid = []        # 3139
     json_list_train = []        # 34468
-
+                                # total number of json files is 37670
+    
     for equipment_type_idx, _ in enumerate(os.listdir(base_path)):
         path = base_path + '/' + os.listdir(base_path)[equipment_type_idx]
         for idx in range(len(os.listdir(path))):
@@ -24,7 +26,7 @@ if __name__ == '__main__':
                 path = path + '/' + os.listdir(path)[idx]
         for _, json_files in enumerate(os.listdir(path)):
             if '3d' not in json_files:
-                json_list_valid.append(path + json_files)
+                json_list_valid.append(path + '/' + json_files)
     # *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
     #
     #
@@ -42,6 +44,63 @@ if __name__ == '__main__':
 
                 for _, json_files in enumerate(os.listdir(day_path)):
                     if '3d' not in json_files:
-                        json_list_train.append(day_path + json_files)
+                        json_list_train.append(day_path + '/' + json_files)
     # *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
-    print(1)
+    #
+    #
+    #
+    #
+    # *-*-*-*-*-*- extracting data from json files *-*-*-*-*-*-
+    json_files_list = json_list_train + json_list_valid
+    exercise_dict = {}
+    counter = defaultdict(int)
+
+    # -*-*-* Var for Debug *-*-*-
+    check_sequences_dict = {}
+    total = 0  # debug
+    # -*-*-*-*-*-*-*-*-*-*-*-*-*-
+
+    for json_file in tqdm(json_files_list, desc='Analyzing JSON Files...', leave=True):
+        with open(json_file , 'r') as f:
+            data = json.load(f)
+
+            # -*-*-* For Debug *-*-*-
+            exercise = data['type_info']['exercise']
+            check_sequences_dict.setdefault(exercise, {})
+            check_sequences_dict[exercise].setdefault('sequences', [])
+            check_sequences_dict[exercise]['sequences'].append(len(data['frames']))
+
+            # -*-*-* Extracting data *-*-*-
+            counter[exercise] += 1
+
+            exercise_dict.setdefault(exercise, {})
+            for num_view in range(1, 6):
+                exercise_dict[exercise].setdefault(f'view{num_view}', {})
+                exercise_dict[exercise][f'view{num_view}'].setdefault(str(counter[exercise]), [])
+
+
+            for frame in range(len(data['frames'])):
+                for num_view in range(1, 6):
+                    exercise_dict[exercise][f'view{num_view}'][str(counter[exercise])].append(data['frames'][frame][f'view{num_view}']['img_key'])
+
+            # view1 --> 1 --> list(A sequence path)
+            #           2 --> list(A sequence path)
+            #           3 --> list(A sequence path)
+            #               .
+            #               .
+            #               .
+            #          end --> list(A sequence path)
+            #               .
+            #               .
+            #               .
+            # view5 --> end --> list(A sequence path)
+
+    print(str(1))
+
+    for k, v in check_sequences_dict.items():
+        print(f"The number of sequences from {k}: {len(v['sequences'])}")
+        total += len(v['sequences'])
+
+    for k,v in exercise_dict.items():
+        for num_view in range(1, 6):
+            total += len(exercise_dict[k][f'view{num_view}'].key())
