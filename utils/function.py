6fd9ea4bf62ced68eef1bd8352802750ce592fb1
SeoJeongYun <tjwjddbs4018@naver.com>
2026-04-01 23:58:03 +0900

    [UPDATE] Extract coordinate of joints

--- utils/function.py
+++ utils/function.py
@@ -74,7 +74,7 @@ def train(config, train_loader, valid_loader, model, criterion, optimizer, epoch
             output = model(input)
 
         # extract coordinate of joints
-        flatten = output.reshape(config.TRAIN.BATCH_SIZE, config.MODEL.NUM_JOINTS, -1).argmax(axis=2)
+        flatten = output.detach().cpu().reshape(config.TRAIN.BATCH_SIZE, config.MODEL.NUM_JOINTS, -1).argmax(axis=2)
         y, x = torch.div(flatten, 256, rounding_mode='floor'), flatten % POSE_RESNET.HEATMAP_SIZE[0]
         coords = torch.stack([x, y], dim=-1)  # [B, J, 2]
 
@@ -83,8 +83,8 @@ def train(config, train_loader, valid_loader, model, criterion, optimizer, epoch
         if isinstance(input, torch.Tensor):
             input = input.cpu().detach().numpy()
             for batch_idx in range(input.shape[0]):
-                img = input[batch_idx].transpose(2, 0).transpose(1, 0)
-                img = img.type(torch.uint8)
+                img = input[batch_idx].transpose(2, 1, 0)
+                img = img.astype(np.uint8)
                 for y, x in coords[batch_idx]:
                     y, x = y.float() / 256 * 1080, x.float() / 256 * 1920
                     plt.scatter(x, y)
